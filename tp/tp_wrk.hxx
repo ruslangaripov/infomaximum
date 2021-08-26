30527006dceef23ff9fcc2c12dfc6a045dddb30e
Ruslan Garipov <ruslanngaripov@gmail.com>
2021-08-26 20:53:02 +0500

    Remove unused stuff
    This removes shutdown event assigned per thread pool work.  We use the
    'Data' structure to store handle to the shutdown event.
    
    Signed-off-by: Ruslan Garipov <ruslanngaripov@gmail.com>

--- tp/tp_wrk.hxx
+++ tp/tp_wrk.hxx
@@ -22,17 +22,6 @@ public:
   {
     SubmitThreadpoolWork(m_wrk);
   }
-  /* Stores the specified 'shutdown' handle which may be used to terminate
-   * **all** instances of this work.  While in general case this function must
-   * not be called from multiple threads simultaneously, I believe it may be
-   * called on x86 architecture with an Intel CPU, which guarantees atomic
-   * writing of a quadword aligned on a 64-bit boundary, and VC++ aligns
-   * quadwords on a 64-boundary by default. */
-  void
-  SetShutdownEvt(void *shutdown) noexcept(true)
-  {
-    m_shutdown = shutdown;
-  }
 
   TP_WORK *
   Get(void) const noexcept(true)
@@ -45,12 +34,6 @@ protected:
   Create(PTP_WORK_CALLBACK clbk, TP_CALLBACK_ENVIRON *env)
   {
     m_wrk = CreateThreadpoolWork(clbk, this, env);
-  }
-
-  void *
-  GetShutdownEvt() const noexcept(true)
-  {
-    return m_shutdown;
   }
 
 private:
